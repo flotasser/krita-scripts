94dcb09382948eab07579b2aa64c9e46587dd764
flotasser <80381745+flotasser@users.noreply.github.com>
2022-06-20 20:50:09 +0200

    Add files via upload
    I wanted an action to automate lighting up the current layer so I can work on a new layer on top and refine my drawing. So by loading my script into the "Ten Scripts" function I can do the following by just hitting Ctr+Shift+1 (you can choose any number) :
    
    1) duplicate the current layer (to save the original layer)
    2) lighten it up and make it grayscale
    3) create a new layer on top to draw the next "pass".

---
+++ Lightbox_simple.py
@@ -0,0 +1,4 @@
+Krita.instance().action('duplicatelayer').trigger()
+Krita.instance().action('krita_filter_normalize').trigger()
+Krita.instance().action('krita_filter_desaturate').trigger()
+Krita.instance().action('add_new_paint_layer').trigger()
