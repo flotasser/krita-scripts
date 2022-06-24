290c3155d6cf35acb7f91efd3fc0471882c88208
flotasser <80381745+flotasser@users.noreply.github.com>
2022-06-24 08:48:17 +0200

    Update Lightbox_simple.py

--- Lightbox_simple.py
+++ Lightbox_simple.py
@@ -1,4 +1,31 @@
-Krita.instance().action('duplicatelayer').trigger()
-Krita.instance().action('krita_filter_normalize').trigger()
-Krita.instance().action('krita_filter_desaturate').trigger()
-Krita.instance().action('add_new_paint_layer').trigger()
+from krita import *
+app = Krita.instance()
+doc = app.activeDocument()
+
+infoFill = InfoObject()
+infoFill.setProperty("color", "#1f97d9")
+select = Selection()
+select.select(0, 0, doc.width(), doc.height(), 255)
+
+fill = doc.createFillLayer("Non-Photo Blue", "color", infoFill, select)
+root = doc.rootNode()
+child = root.childNodes()
+
+#####
+
+# Still need a check if "Non-Photo Blue" exists, if yes -> delete the old one. Or keep it and move it, whatever is easier. 
+# Any maybe an iterative name for new "Ink" layers would be great.
+
+#####
+
+root.addChildNode(fill, child[0])
+
+# create Fill Layer: Non-Photo Blue
+layerBlueFill = doc.nodeByName("Non-Photo Blue")
+layerBlueFill.setBlendingMode("screen")
+
+# create Paint Layer: Ink
+paint = doc.createNode("Ink", "paintlayer")
+root.addChildNode(paint, doc.nodeByName("Non-Photo Blue"))
+
+doc.refreshProjection()
