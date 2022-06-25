70d374cf41c88d5a89e070b3885ae5d3dcf2ee1e
flotasser <80381745+flotasser@users.noreply.github.com>
2022-06-25 12:24:07 +0200

    Update Lightbox.py

--- Lightbox.py
+++ Lightbox.py
@@ -16,18 +16,21 @@ topNode = current.parentNode()
 
 # setting up the information needed for the blue fill layer
 infoFill = InfoObject()
-infoFill.setProperty("color", "#1f97d9")
+infoFill.setProperty("color", "#0f87e6")
 select = Selection()
 
 # telling the application what region will be filled exactly, everything in this case
 select.select(0, 0, doc.width(), doc.height(), 255)
 
+# fill layer
 fill = doc.createFillLayer("Non-Photo Blue", "color", infoFill, select)
 
-# Take the current Painting Layers Opacity down to 35% before creating a new one
+# Take the current Painting Layers Opacity down to 50% before creating a new one
+current_op = doc.activeNode().opacity()
 if not doc.activeNode() == doc.nodeByName("Background"):
-	doc.activeNode().setOpacity(89)
-
+	doc.activeNode().setOpacity(current_op*0.5)
+	doc.activeNode().setBlendingMode("multiply")
+	
 # Is there a 'Non-Photo Blue Layer'? If yes - delete before creating a new one
 if doc.nodeByName("Non-Photo Blue"):
  	doc.nodeByName("Non-Photo Blue").remove()
